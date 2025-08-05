c0e24e8f5f6bebc653967408066f450c12c91c81
zeon0xx0 <jihyon5615@naver.com>
2025-08-05 15:42:10 +0900

    1539

---
+++ src/main/java/pack/Spring03Application.java
@@ -0,0 +1,20 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Spring03Application {
+	@GetMapping("/")
+	public String tiger() {
+		return "String03:1539";
+	}
+ 
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03Application.class, args);
+	}
+
+}
---
+++ src/test/java/pack/Spring03ApplicationTests.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
